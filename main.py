a5b1de9ae328e229228705007ea159b0c51e5055
Youp Verkooijen <youpverkooijen@gmail.com>
2024-03-21 11:39:11 +0100

    Update device configuration and fix swipe direction

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pykinect_azure.k4a._k4atypes import k4a_float3_t
 import threading
 from pykinect_azure.k4a.configuration import Configuration
 import pykinect_azure.k4a._k4a as _k4a
+import numpy as np
 
 pyautogui.FAILSAFE = False  # Disable the fail-safe feature
 
@@ -13,20 +14,17 @@ screen_width, screen_height = pyautogui.size()
 screen_size = pyautogui.size()
 
 SWIPE_SPEED_THRESHOLD = 1000
-SWIPE_COOLDOWN = 2
+SWIPE_COOLDOWN = 1.3
 last_swipe_time = 0
-right_wrist_history = []
 
 
 # Constants for exponential smoothing
 SMOOTHING_FACTOR = 0.7  # Adjust as needed for desired smoothing effect
 
-
-import numpy as np
-
 # Define a threshold for movement (in pixels)
 MOVEMENT_THRESHOLD = 10  # Adjust as needed
 
+right_wrist_history = []
 
 def update_cursor_position(mapped_x, mapped_y, previous_position):
     global smoothed_position
@@ -145,17 +143,12 @@ if __name__ == "__main__":
     pykinect.initialize_libraries(track_body=True)
     last_move_time = None
 
-    device_config = pykinect.default_configuration
-    device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_1080P
+    device_config = Configuration()
+    device_config.color_resolution = pykinect.K4A_COLOR_RESOLUTION_1440P
     device_config.color_format = pykinect.K4A_IMAGE_FORMAT_COLOR_BGRA32
-    device_config.depth_mode = pykinect.K4A_DEPTH_MODE_WFOV_2X2BINNED
-
-    default_configuration = Configuration()
-    default_configuration.depth_mode = (
-        _k4a.K4A_DEPTH_MODE_NFOV_UNBINNED
-    )  # or _k4a.K4A_DEPTH_MODE_WFOV_UNBINNED
-    default_configuration.camera_fps = _k4a.K4A_FRAMES_PER_SECOND_30
-    default_configuration.synchronized_images_only = True
+    device_config.depth_mode = pykinect.K4A_DEPTH_MODE_NFOV_UNBINNED
+    device_config.camera_fps = pykinect.K4A_FRAMES_PER_SECOND_30
+    device_config.synchronized_images_only = True
 
     device = pykinect.start_device(config=device_config)
     body_tracker = pykinect.start_body_tracker()
@@ -239,12 +232,12 @@ if __name__ == "__main__":
                                 elif vertical_speed > SWIPE_SPEED_THRESHOLD:
                                     print("Swipe down detected")
                                     # press down arrow key
-                                    pyautogui.press("up")
+                                    pyautogui.press("down")
                                     last_swipe_time = current_time
                                 elif vertical_speed < -SWIPE_SPEED_THRESHOLD:
                                     print("Swipe up detected")
                                     # press up arrow key
-                                    pyautogui.press("down")
+                                    pyautogui.press("up")
                                     last_swipe_time = current_time
                             right_wrist_history = []
 
